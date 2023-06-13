1b4f91a1538ddcf4004d828393a013a83ba87c74
crowdbotics-apps <team@crowdbotics.com>
2023-06-13 13:20:15 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -2,11 +2,10 @@ from django.conf import settings
 from django.db import models
 class Pet(models.Model):
     'Generated Model'
-    name = models.CharField(max_length=200,)
+    name = models.CharField(max_length=100,)
     type = models.CharField(max_length=100,)
-    image_url = models.CharField(max_length=200,)
+    image_url = models.CharField(max_length=255,)
     attributes = models.TextField()
-    is_favorite = models.BooleanField(default="False",)
+    is_favorite = models.BooleanField()
     description = models.TextField()
-    date_adopted = models.DateField(auto_now_add=True,)
-    user = models.ForeignKey("users.User",on_delete=models.CASCADE,null=True,blank=True,related_name="pet_user",)
+    date_adopted = models.DateTimeField(auto_now_add=True,)
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserUpdateView, user_redirect_view, user_detail_view, user_update_view, UserDetailView
+from .views import user_redirect_view, UserUpdateView, user_update_view, user_detail_view, UserDetailView
 
 app_name = "users"
 urlpatterns = [
